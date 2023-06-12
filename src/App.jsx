4bf42729f949ba493697a3f5d40beeb0ec94e45e
kalil365 <kalilwebmaster@gmail.com>
2023-06-12 22:54:17 +0100

    gitadd-google-maps-project

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { Fragment } from "react";
+import {
+  GoogleMap,
+  useLoadScript,
+} from "@react-google-maps/api";
+
+import "./App.css";
+
+function App() {
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: import.meta.env.VITE_MAP_API_KEY,
+  });
+
+  return (
+    <Fragment>
+      <div className="container">
+        <h1 className="text-center">Vite + React | Google Map Markers</h1>
+        <div style={{ height: "90vh", width: "100%" }}>
+          {isLoaded ? (
+            <GoogleMap
+              center={{ lat: 36.848951, lng: 10.158502 }}
+              zoom={10}
+              mapContainerStyle={{ width: "100%", height: "90vh" }}
+            >
+            
+            </GoogleMap>
+          ) : null}
+        </div>
+      </div>
+    </Fragment>
+  );
+}
+
+export default App;
